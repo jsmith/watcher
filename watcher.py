acd4ba958511961b22e08a5b69d23322f26c74e4
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-18 18:12:54 -0300

    updating algorithm

--- watcher.py
+++ watcher.py
@@ -5,69 +5,18 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import bs4
 import yaml
 from selenium import webdriver
 
-with open(os.path.join(os.path.expanduser('~'), '.watcher.yaml')) as f:
-    config = yaml.load(f)
-
-email = config['email']
-password = config['password']
-
-del config['email']
-del config['password']
-
-
-IGNORE = ['[document]', 'html', 'head', 'script', 'style', 'body', 'meta', 'title', 'style', 'div', 'noscript']
-
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    format="%(asctime) %(levelname)-5.5s: %(message)s",
     handlers=[logging.StreamHandler()]
 )
 logger = logging.getLogger(__name__)
 
 
-def same(new_soup, old_soup):
-    new_children = new_soup.findChildren()
-    old_children = old_soup.findChildren()
-
-    # We can't just compare lengths the zip the children together. Sometimes pages will have different lengths but the same content.
-    new = 0
-    old = 0
-    while True:
-        if new >= len(new_children) or old >= len(old_children):
-            break
-
-        new_element = new_children[new]
-        old_element = old_children[old]
-
-        new += 1
-        old += 1
-
-        if new_element.name != old_element.name:
-            if len(new_children) > len(old_children):
-                new += 1
-                continue
-            elif len(old_children) > len(new_children):
-                old += 1
-                continue
-
-            logger.info('New element name ({}) does not match old element name ({})!'.format(new_element.name, old_element.name))
-            return False
-
-        if new_element.name in IGNORE:
-            continue
-
-        if new_element.text.strip() != old_element.text.strip():
-            logger.info('Found mismatch element:\n{}\nvs.{}'.format(old, new_element.text, old_element.text))
-            return False
-
-    return True
-
-
-def send_email(url):
+def send_email(url, email, password):
     destination = ['jacob.smith@unb.ca']
 
     msg = MIMEMultipart()
@@ -89,42 +38,48 @@ def main():
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', chrome_options=options)
+
     while True:
-        for category_name in config:
-            category = config[category_name]
+        file = os.path.join(os.path.expanduser('~'), '.watcher.yaml')
+        with open(file) as f:
+            config = yaml.load(f)
 
-            for url_name in category:
-                url = category[url_name]
-                logger.info('Checking {}'.format(url))
+        email = config['email']
+        password = config['password']
+        sites = config['sites']
 
-                path = os.path.join(os.path.expanduser('~'), '.watcher', category_name, url_name)
-                if not os.path.exists(path):
-                    previous_contents = None
-                else:
-                    with open(path) as f:
-                        previous_contents = f.read()
+        for name in sites:
+            information = sites[name]
+            if 'counts' not in information:
+                information['counts'] = {}
 
-                driver.get(url)
-                contents = driver.page_source
+            url = information['url']
+            keys = information['keys']
+            previous_counts = information['counts']
 
-                if previous_contents is None:
-                    logger.info('Previous contents of {} is None. Writing contents to {}!'.format(url, path))
-                    os.makedirs(os.path.dirname(path), exist_ok=True)
-                    with open(path, 'w') as f:
-                        f.write(contents)
+            logger.info('Checking {}'.format(url))
 
+            driver.get(url)
+            contents = driver.page_source
+
+            diffs = {}
+            for key in keys:
+                previous_count = previous_counts.get(key)
+                count = contents.count(key)
+
+                if previous_count is None:
+                    logger.info('Previous count of {} is None. Writing {} as previous count.'.format(key, count))
+                    previous_counts[key] = count
                     continue
 
-                old_soup = bs4.BeautifulSoup(previous_contents, "html.parser")
-                new_soup = bs4.BeautifulSoup(contents, "html.parser")
-                if same(new_soup, old_soup):
-                    continue
+                if previous_count != count:
+                    diffs[key] = abs(previous_count - count)
 
-                with open(path, 'w') as f:
-                    logger.info('Overwriting contents of {}'.format(path))
-                    f.write(contents)
+            if diffs:
+                send_email(url, email, password)
 
-                send_email(url)
+        with open(os.path.join(os.path.expanduser('~'), '.watcher.yaml'), 'w') as f:
+            yaml.dump(config, f)
 
         logger.info('Sleeping :)')
         days = 1
@@ -133,4 +88,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
